09c8b74fe1e195a05205464c342530bd760b67ae
manahil12mahmood <59745468+manahil12mahmood@users.noreply.github.com>
2023-02-25 15:12:06 +0500

    Create age.cpp

---
+++ age.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"please eneter age: ";
+    int age;
+    
+     cin>> age; 
+     if  ( age>= 18){
+    cout<<"elegible to vote ";
+     }
+     
+  else
+    {
+    cout<<" not";
+}
+    return 0;
+}
