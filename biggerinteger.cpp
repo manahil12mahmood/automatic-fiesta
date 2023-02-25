e05ebb50117adf132a6be3961e65e529e9b1707b
manahil12mahmood <59745468+manahil12mahmood@users.noreply.github.com>
2023-02-25 15:16:01 +0500

    Update and rename age.cpp to biggerinteger.cpp

---
+++ biggerinteger.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"please enter two integers: ";
+    int a;
+    int b;
+    
+     cin>> a;
+cin >> b;
+if  ( a> b){
+    cout<<"first integer is bigger";
+     }
+     
+  else
+    {
+    cout<<" second integer is bigger";
+}
+    return 0;
+}
